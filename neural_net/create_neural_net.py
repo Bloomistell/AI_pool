bbf1edd9de69d41a72d8f490532b2f660c63639b
JulesPot <76julesp@gmail.com>
2023-02-08 21:02:33 +0100

    neural environment

--- neural_net/create_neural_net.py
+++ neural_net/create_neural_net.py
@@ -1,7 +1,21 @@
 import torch
+from torch import nn
 
-class CreateNeuralNet():
-    def __init__(self):
-        pass
 
-    
+class DQN(nn.Module):
+
+    def __init__(self, n_observations, layers, n_actions):
+        super(DQN, self).__init__()
+
+        layer_list = [nn.Linear(n_observations, layers[0])]
+        for i in range(len(layers)-1):
+            layer_list.append(nn.ReLU())
+            layer_list.append(nn.Linear(layers[i], layers[i+1]))
+
+        layer_list.append(nn.ReLU())
+        layer_list.append(nn.Linear(layers[-1], n_actions))
+
+        self.network = nn.Sequential(layer_list)
+
+    def forward(self, x):
+        return self.network(x)
